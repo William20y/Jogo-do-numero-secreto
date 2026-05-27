f446b4bf082b68545d0b5fc7cff546802b04d321
William Rodrigues <miguewi2223@hotmail.com>
2026-05-27 19:02:49 -0300

    texto com a sintese errada!

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ function verificarChute() {
     if(chute == numeroSecreto){
         exibirTextoNaTela('h1','Acertou!');
         let palavratentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `Voce descobriu o numero secreto com ${tentativas} ${palavratentativa}`
+        let mensagemTentativas = `Você descobriu o numero secreto com ${tentativas} ${palavratentativa}`
         exibirTextoNaTela('p',mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
 
